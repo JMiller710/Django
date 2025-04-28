e3ac86cccc36118e7380d47598a5d3ac90b0f0c9
[JMiller710] <[jmill322@uncc.edu]>
2025-04-28 16:17:25 -0400

    Django Part 3

--- OneDrive/Desktop/studybud/studybud/settings.py
+++ OneDrive/Desktop/studybud/studybud/settings.py
@@ -39,16 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
-]
+
+    'rest_framework',
+    "corsheaders",
+    ]
+
+AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -120,10 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR/ 'static'
 ]
+
+
+MEDIA_ROOT = BASE_DIR / 'static/styles/images'
 
 # STATIC_ROOT = 
 
@@ -131,3 +144,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
